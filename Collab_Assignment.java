8e65eff8dcdb73a22fa39d51d645bb0602b001a7
Jonathan Fuchs <jfuchs@seas.upenn.edu>
2019-06-20 09:55:27 -0400

    changed wording of request for user name; closed scanner

--- Collab_Assignment.java
+++ Collab_Assignment.java
@@ -5,11 +5,11 @@ public class Collab_Assignment {
 	public static void main(String[] args) {
 		Scanner userInputScanner = new Scanner(System.in);
 
-		System.out.println("Hello! What is your name?");
+		System.out.println("Hello! Please tell me your name: ");
 		String name = userInputScanner.nextLine();
 		System.out.println("Hello " + name +"!");
 		
-		
+		userInputScanner.close();
 	}
 
 }
