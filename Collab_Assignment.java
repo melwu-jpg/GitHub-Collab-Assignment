fe9c86d8bc8d727d6045fd31ee4a6310048c4882
melwu-jpg <melwu@seas.upenn.edu>
2019-06-20 10:12:20 -0400

    Resolving the conflict

--- Collab_Assignment.java
+++ Collab_Assignment.java
@@ -5,7 +5,7 @@ public class Collab_Assignment {
 	public static void main(String[] args) {
 		Scanner userInputScanner = new Scanner(System.in);
 
-		System.out.println("Hello! Please tell me your name: ");
+		System.out.println("Hello! Por favor tell me your name: ");
 		String name = userInputScanner.nextLine();
 		System.out.println("Hello " + name +"!");
 		
