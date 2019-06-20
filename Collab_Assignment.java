9c856c9d45b03fdfc0a56b903b76f657bcef608c
melwu-jpg <melwu@seas.upenn.edu>
2019-06-19 20:58:45 -0400

    Add existin file

---
+++ Collab_Assignment.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Collab_Assignment {
+
+	public static void main(String[] args) {
+		Scanner userInputScanner = new Scanner(System.in);
+
+		System.out.println("Hello! What is your name?");
+		String name = userInputScanner.nextLine();
+		System.out.println("Hello " + name +"!");
+		
+		
+	}
+
+}
